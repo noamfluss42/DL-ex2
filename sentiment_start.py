b0a797d99d7b14b5b4a3eb236a338f227228cdab
noam fluss <noam.fluss.42@gmail.com>
2021-12-09 12:21:31 +0200

    add automatization

--- sentiment_start.py
+++ sentiment_start.py
@@ -16,30 +16,20 @@ import numpy as np
 import loader as ld
 import pandas as pd
 import matplotlib
-
+import wandb
 from matplotlib import pyplot as plt
 
 plt.style.use('ggplot')
 matplotlib.rcParams['font.size'] = 14
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'  # TODO delete
 
-batch_size = 64
-output_size = 2
-hidden_size = 128  # to experiment with
-
-run_recurrent = False  # else run Token-wise MLP
-use_RNN = True  # otherwise GRU
-atten_size = 0  # atten > 0 means using restricted self atten
-
 reload_model = False
-num_epochs = 1  # 10 is the original number
-learning_rate = 0.0001
+
 test_interval = 100
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# Loading sataset, use toy = True for obtaining a smaller dataset
 
-train_dataset, test_dataset, num_words, input_size = ld.get_data_set(batch_size)
-our_test_dataset = ld.get_our_test_data_set(batch_size)
+
+# Loading sataset, use toy = True for obtaining a smaller dataset
 
 
 # Special matrix multipication layer (like torch.Linear but can operate on arbitrary sized
@@ -65,15 +55,17 @@ class MatMul(nn.Module):
 # Implements RNN Unit
 
 class ExRNN(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size):
+    def __init__(self, layers_dim):
         super(ExRNN, self).__init__()
-        self.hidden_size = hidden_size
+        self.input_size = layers_dim[0]
+        self.hidden_size = layers_dim[1]
+        self.output_size = layers_dim[-1]
         self.sigmoid = torch.sigmoid
 
         # RNN Cell weights
-        self.in2hidden = nn.Linear(input_size + hidden_size, hidden_size)
+        self.in2hidden = nn.Linear(self.input_size + self.hidden_size, self.hidden_size)
         # what else?
-        self.in2output = nn.Linear(hidden_size, output_size)
+        self.in2output = nn.Linear(self.hidden_size, self.output_size)
 
     def name(self):
         return "RNN"
@@ -92,18 +84,17 @@ class ExRNN(nn.Module):
 # Implements GRU Unit
 
 class ExGRU(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size):
+    def __init__(self, layers_dim):
         super(ExGRU, self).__init__()
-        self.hidden_size = hidden_size
-        # GRU Cell weights
-        # self.something =
-        # etc ...
+        self.input_size = layers_dim[0]
+        self.hidden_size = layers_dim[1]
+        self.output_size = layers_dim[-1]
         self.sigmoid = torch.sigmoid
         self.tanh = torch.tanh
-        self.zt_linear = nn.Linear(input_size + hidden_size, hidden_size)
-        self.rt_linear = nn.Linear(input_size + hidden_size, hidden_size)
-        self.ht_tilda_linear = nn.Linear(input_size + hidden_size, hidden_size)
-        self.hidden_to_output = nn.Linear(hidden_size, output_size)
+        self.zt_linear = nn.Linear(self.input_size + self.hidden_size, self.hidden_size)
+        self.rt_linear = nn.Linear(self.input_size + self.hidden_size, self.hidden_size)
+        self.ht_tilda_linear = nn.Linear(self.input_size + self.hidden_size, self.hidden_size)
+        self.hidden_to_output = nn.Linear(self.hidden_size, self.output_size)
 
     def name(self):
         return "GRU"
@@ -122,75 +113,81 @@ class ExGRU(nn.Module):
 
 
 class ExMLP(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size):
+    def __init__(self,
+                 layers_dim,
+                 layers_activations):
         super(ExMLP, self).__init__()
-
-        self.ReLU = torch.nn.ReLU()
+        self.input_size = layers_dim[0]
+        self.output_size = layers_dim[-1]
+        self.layers_activations = layers_activations
         self.sigmoid = torch.sigmoid
-        # Token-wise MLP network weights
-        self.layer1 = MatMul(input_size, hidden_size)
-        # additional layer(s)
-        self.layer2 = MatMul(hidden_size, 256)
-
-        self.layer3 = MatMul(256, output_size)
+        self.layers = []
+        # Token-wise MLP + Restricted Attention network implementation
+        for i in range(len(layers_dim) - 1):
+            self.layers.append(MatMul(layers_dim[i], layers_dim[i + 1]))
+        self.layers = nn.ModuleList(self.layers)
 
     def name(self):
         return "MLP"
 
     def forward(self, x):
         # Token-wise MLP network implementation
-
-        x = self.ReLU(self.layer1(x))
-        # rest
-        x = self.ReLU(self.layer2(x))
-        x = self.sigmoid(self.layer3(x))
+        for layer_index in range(len(self.layers)):
+            x = self.layers[layer_index](x)
+            x = self.layers_activations[layer_index](x)
         return x
 
 
 class ExLRestSelfAtten(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size):
+    def __init__(self, atten_size,
+                 layers_dim,
+                 layers_activations, atten_layer_index):
         super(ExLRestSelfAtten, self).__init__()
 
-        self.input_size = input_size
-        self.output_size = output_size
+        hidden_size = layers_dim[atten_layer_index]
+        self.input_size = layers_dim[0]
+        self.output_size = layers_dim[-1]
         self.sqrt_hidden_size = np.sqrt(float(hidden_size))
-        self.ReLU = torch.nn.ReLU()
+        self.layers_activations = layers_activations
+        self.atten_size = atten_size
         self.softmax = torch.nn.Softmax(2)
-        self.sigmoid = torch.sigmoid
+        self.layers = []
         # Token-wise MLP + Restricted Attention network implementation
+        for i in range(len(layers_dim) - 1):
+            self.layers.append(MatMul(layers_dim[i], layers_dim[i + 1]))
 
-        self.layer1 = MatMul(input_size, hidden_size)
-        self.W_q = MatMul(hidden_size, hidden_size, use_bias=False)
-        self.W_k = MatMul(hidden_size, hidden_size, use_bias=False)
-        self.W_v = MatMul(hidden_size, hidden_size, use_bias=False)
-        self.layer2 = MatMul(hidden_size, output_size)
-        # rest ...
+        self.W_q = MatMul(layers_dim[atten_layer_index], layers_dim[atten_layer_index], use_bias=False)
+        self.W_k = MatMul(layers_dim[atten_layer_index], layers_dim[atten_layer_index], use_bias=False)
+        self.W_v = MatMul(layers_dim[atten_layer_index], layers_dim[atten_layer_index], use_bias=False)
+
+        self.atten_layer_index = atten_layer_index
+
+        self.layers = nn.ModuleList(self.layers)
 
     def name(self):
         return "MLP_atten"
 
     def forward(self, x):
         # Token-wise MLP + Restricted Attention network implementation
+        for layer_index in range(self.atten_layer_index):
+            x = self.layers[layer_index](x)
+            x = self.layers_activations[layer_index](x)
 
-        x = self.layer1(x)
-        x = self.ReLU(x)
-
-        # generating x in offsets between -atten_size and atten_size 
+        # generating x in offsets between -atten_size and atten_size
         # with zero padding at the ends
 
-        padded = pad(x, (0, 0, atten_size, atten_size, 0, 0))
+        padded = pad(x, (0, 0, self.atten_size, self.atten_size, 0, 0))
 
         x_nei = []
-        for k in range(-atten_size, atten_size + 1):
+        for k in range(-self.atten_size, self.atten_size + 1):
             x_nei.append(torch.roll(padded, k, 1))
 
         x_nei = torch.stack(x_nei, 2)
-        x_nei = x_nei[:, atten_size:-atten_size, :]
+        x_nei = x_nei[:, self.atten_size:-self.atten_size, :]
 
         # x_nei has an additional axis that corresponds to the offset
 
         # Applying attention layer
-        N = 128  # TODO change to 100?
         query = self.W_q(x_nei)
         keys = self.W_k(x_nei)
         vals = self.W_v(x_nei)
@@ -203,9 +200,11 @@ class ExLRestSelfAtten(nn.Module):
         alpha = self.softmax(d)
         weighted_values = torch.bmm(alpha, vals_view)
         output_view = torch.sum(weighted_values, 1)
-        output_atten = output_view.view(x.shape[0], 100, output_view.shape[1])
-        x = self.layer2(output_atten)
-        x = self.sigmoid(x)
+        x = output_view.view(x.shape[0], 100, output_view.shape[1])
+
+        for layer_index in range(self.atten_layer_index, len(self.layers)):
+            x = self.layers[layer_index](x)
+            x = self.layers_activations[layer_index](x)
         return x, alpha
 
 
@@ -231,8 +230,8 @@ def get_accuracy_score(output: torch.tensor, labels: torch.tensor):
     :param labels: the original labels, tensor[samples,labels]
     :return: accuracy score
     """
-    output_array = output.detach().numpy().argmax(axis=1)
-    labels_array = labels.detach().numpy().argmax(axis=1)
+    output_array = output.cpu().detach().numpy().argmax(axis=1)
+    labels_array = labels.cpu().detach().numpy().argmax(axis=1)
     return np.where(output_array == labels_array)[0].size / labels_array.size
 
 
@@ -257,7 +256,7 @@ def plot_acc(test_acc, model_name):
     plt.show()
 
 
-def print_test_accuracy(model, test_dataset, run_recurrent, atten_size):
+def get_test_accuracy(model, test_dataset, run_recurrent, atten_size,num_words):
     acc = 0
     output = 0
     for labels, reviews, reviews_text in test_dataset:
@@ -276,53 +275,57 @@ def print_test_accuracy(model, test_dataset, run_recurrent, atten_size):
 
             output = torch.mean(sub_score, 1)
         acc += get_accuracy_score(output, labels)
-        if not run_recurrent:
-            nump_subs = sub_score.detach().numpy()
-            labels = labels.detach().numpy()
 
-    print(f"Final accuracy score = {acc / len(test_dataset)}")
+    return acc / len(test_dataset)
 
 
-def print_sub_score_words(model, our_test_dataset):
+def print_sub_score_words(model, our_test_dataset, atten_size):
     for labels, reviews, reviews_text in our_test_dataset:
-        sub_score = model(reviews)
+        if atten_size > 0:
+            sub_score, atten_weights = model(reviews)
+        else:
+            sub_score = model(reviews)
         output = torch.mean(sub_score, 1)
         print("start print_review")
-        nump_subs = sub_score.detach().numpy()
-        labels_argmax = labels.detach().numpy().argmax(axis=1)
-        nump_output_argmax = output.detach().numpy().argmax(axis=1)
+        nump_subs = sub_score.cpu().detach().numpy()
+        labels_argmax = labels.cpu().detach().numpy().argmax(axis=1)
+        nump_output_argmax = output.cpu().detach().numpy().argmax(axis=1)
         print(f"labels_argmax = {labels_argmax}\noutput_argmax = {nump_output_argmax}")
         idx_first = 0
-        first_predict, first_label = labels_argmax[idx_first],labels_argmax[idx_first]
+        first_predict, first_label = labels_argmax[idx_first], labels_argmax[idx_first]
         idx_second = 1
         second_predict, second_label = nump_output_argmax[idx_second], labels_argmax[idx_second]
-        print_review(reviews_text[idx_first], nump_subs[idx_first, :, 0], nump_subs[idx_first, :, 1], first_label, first_predict)
+        print_review(reviews_text[idx_first], nump_subs[idx_first, :, 0], nump_subs[idx_first, :, 1], first_label,
+                     first_predict)
         print()
         print_review(reviews_text[idx_second], nump_subs[idx_second, :, 0], nump_subs[idx_second, :, 1], second_label,
                      second_predict)
 
 
-if __name__ == "__main__":
+def choose_model(run_recurrent, use_RNN, atten_size, layers_dim, layers_activations, atten_layer_index):
     if run_recurrent:
         if use_RNN:
-            model = ExRNN(input_size, output_size, hidden_size)
+            model = ExRNN(layers_dim)
         else:
-            model = ExGRU(input_size, output_size, hidden_size)
+            model = ExGRU(layers_dim)
     else:
         if atten_size > 0:
-            model = ExLRestSelfAtten(input_size, output_size, hidden_size)
+            model = ExLRestSelfAtten(atten_size,
+                                     layers_dim,
+                                     layers_activations, atten_layer_index)
         else:
-            model = ExMLP(input_size, output_size, hidden_size)
+            model = ExMLP(layers_dim, layers_activations)
 
     print("Using model: " + model.name())
 
     if reload_model:
         print("Reloading model")
-        model = torch.load(model.name() + ".pth")
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        model.load_state_dict(torch.load(model.name() + ".pth"))
     model.to(device)
+    return model
+
+
+def train(train_dataset, test_dataset, num_words, num_epochs, run_recurrent, atten_size, model, criterion, optimizer):
     train_loss = 1.0
     test_loss = 1.0
 
@@ -375,6 +378,8 @@ if __name__ == "__main__":
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
+                output_train = output
+                labels_train = labels
 
             # averaged losses
             if test_iter:
@@ -382,25 +387,100 @@ if __name__ == "__main__":
                 train_loss_list.append(train_loss)
                 test_loss_list.append(test_loss)
                 acc = get_accuracy_score(output, labels)
-                test_acc_list.append(acc)
-            else:
-                train_loss = 0.9 * float(loss.detach()) + 0.1 * train_loss
+                acc_train = get_accuracy_score(output_train, labels_train)
 
-            if test_iter:
+                test_acc_list.append(acc)
                 print(
                     f"Epoch [{epoch + 1}/{num_epochs}], "
                     f"Step [{itr + 1}/{len(train_dataset)}], "
                     f"Train Loss: {train_loss:.4f}, "
                     f"Test Loss: {test_loss:.4f}, "
                     f"Test Accuracy: {acc}"
+                    f"train Accuracy: {acc_train}"
                 )
+                # wandb.log({"interval step batch test loss": test_loss})
+                # wandb.log({"interval step batch train loss": train_loss})
+                # wandb.log({"interval step test acc": acc})
+                # wandb.log({"interval step train acc": acc_train})
+            else:
+                train_loss = 0.9 * float(loss.detach()) + 0.1 * train_loss
 
+        # wandb.log({"epoch step test acc": get_test_accuracy(model, test_dataset, run_recurrent, atten_size)})
         # saving the model
-        torch.save(model, model.name() + ".pth")
+    torch.save(model, model.name() + ".pth")
+    return train_loss_list, test_loss_list, test_acc_list
 
+
+def run_by_architecture(train_dataset, test_dataset, num_words, our_test_dataset,
+                        learning_rate=0.0001,
+                        num_epochs=2,
+                        run_recurrent=False,
+                        use_RNN=True,
+                        atten_size=5,
+                        layers_dim=(100, 128, 2),
+                        layers_activations=(torch.nn.ReLU(), torch.nn.ReLU(), torch.sigmoid),
+                        atten_layer_index=1):
+    model = choose_model(run_recurrent, use_RNN, atten_size, layers_dim, layers_activations, atten_layer_index)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    train_loss_list, test_loss_list, test_acc_list = train(train_dataset, test_dataset, num_words, num_epochs,
+                                                           run_recurrent, atten_size, model, criterion, optimizer)
     plot_loss(train_loss_list, test_loss_list, model.name())
     plot_acc(test_acc_list, model.name())
-    print_test_accuracy(model, test_dataset, run_recurrent, atten_size)
-
+    print(f"Final accuracy score = {get_test_accuracy(model, test_dataset, run_recurrent, atten_size,num_words)}")
     print_sub_score_words(model, our_test_dataset)  # Question 2 - print sub scores MLP
-    print('!')
+
+
+def main():
+    learning_rate = 0.0001
+    num_epochs = 1  # 10 is the original number
+
+    batch_size = 64
+
+    run_recurrent = False  # else run Token-wise MLP
+    use_RNN = True  # otherwise GRU
+    atten_size = 5  # atten > 0 means using restricted self atten
+
+    train_dataset, test_dataset, num_words, input_size = ld.get_data_set(batch_size)
+    our_test_dataset = ld.get_our_test_data_set(batch_size)
+
+    layers_activations = (torch.nn.ReLU(), torch.nn.ReLU(), torch.nn.ReLU(), torch.sigmoid)
+    atten_layer_index = 1
+    # for hidden_size in [64]:
+    #     layers_dim = (100, hidden_size, 256, 2),
+    #     config = {
+    #         "learning_rate": learning_rate,
+    #         "epochs": num_epochs,
+    #         "batch_size": batch_size,
+    #         "run_recurrent": run_recurrent,
+    #         "use_RNN": use_RNN,
+    #         "attention_after_layer": atten_layer_index,
+    #         "layer1_size": layers_dim[0],
+    #         "layer1_activation": "RELU",
+    #         "layer2_size": layers_dim[1],
+    #         "layer2_activation": "RELU",
+    #         "layer3_size": layers_dim[2],
+    #         "layer3_activation": "RELU",
+    #         "layer4_size": layers_dim[3],
+    #         "layer4_activation": "sigmoid",
+    #     }
+    #     wandb.init(project="DL-ex2_with_documentation_delete", entity="noam-fluss", config=config)
+    run_by_architecture(train_dataset, test_dataset, num_words, our_test_dataset,
+                        learning_rate,
+                        num_epochs,
+                        run_recurrent,
+                        use_RNN,
+                        atten_size,
+                        layers_dim,
+                        layers_activations,
+                        atten_layer_index)
+
+
+if __name__ == "__main__":
+    config = {
+        "learning_rate": learning_rate,
+        "epochs": num_epochs,
+        "batch_size": batch_size
+    }
+    # wandb.init(project="DL-ex2", entity="noam-fluss", config=config)
+    main()
